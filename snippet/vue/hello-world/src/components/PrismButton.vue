df2b66ae975e91bf9fb152b9505b3714a2f5a437
Patrick Ratzow <patrickratzow@hotmail.dk>
2024-08-21 02:14:25 +0200

    Add Vue Hello World demo

---
+++ snippet/vue/hello-world/src/components/PrismButton.vue
@@ -0,0 +1,37 @@
+<script setup>
+const click = () => {
+    if (!window.$prism) {
+      alert('Prism SDK is not loaded');
+
+      return;
+    }
+
+    try {
+      window.$prism.submission.init();
+
+      alert('Successfully called out to the Prism SDK');
+    } catch (e) {
+      alert('Failed to call out to the Prism SDK. Error: ' + e);
+    }
+}
+</script>
+
+<template>
+    <button @click="click">Verify Prism SDK</button>
+</template>
+
+<style scoped>
+button {
+    background-color: hsla(160, 100%, 37%, 1);;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    border-radius: 8px;
+    cursor: pointer;
+}
+</style>
---
+++ snippet/vue/hello-world/src/components/TitleBar.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  } 
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>Patient Prism demo</h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 3.6rem;
+  position: relative;
+  top: -5px;
+}
+
+h3 {
+  font-size: 1.4rem;
+  position: relative;
+  top: -15px;
+  left: 5px
+}
+
+.greetings h1 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
