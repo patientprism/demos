d2ca1dde5fca495f00eb57e56c1188624b03a44d
Patrick Ratzow <patrickratzow@hotmail.dk>
2024-08-21 03:20:47 +0200

    Add Vue TypeScript Forms project

---
+++ snippet/vue/forms-typescript/README.md
@@ -0,0 +1,19 @@
+# Submission Forms
+
+This demo demostrates how to use the submission send functionality of the Prism SDK with a form.
+
+This demo uses the NPM package `@patientprism/snippet-sdk`. To install this in your own project, use `npm install --save-dev @patientprism/snippet-sdk`
+
+If you want to test with your own Code Snippet, replace the `<script>` tag for the snippet in the `index.html` file for this demo.
+
+![Picture](image.png)
+
+Which results in the following in the Patient Prism platform
+![Result](https://i.imgur.com/3kjnsCp.png)
+
+## Run Demo
+
+```sh
+npm install
+npm run dev
+```
---
+++ snippet/vue/forms-typescript/src/components/TitleBar.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+type Props = {
+  msg: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>Patient Prism demo</h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 3.6rem;
+  position: relative;
+  top: -5px;
+}
+
+h3 {
+  font-size: 1.4rem;
+  position: relative;
+  top: -15px;
+  left: 5px
+}
+
+.greetings {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.greetings h1 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings {
+    align-items: flex-start;
+  }
+  
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
